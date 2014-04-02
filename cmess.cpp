56f479f479afcaf1957a4baecbc710c905b308d1
mrlaker <sysulaker@gmail.com>
2014-04-02 22:50:30 +0800

    Output logical operator changing succeed

--- cmess.cpp
+++ cmess.cpp
@@ -25,15 +25,19 @@
 #include "clang/Rewrite/Frontend/Rewriters.h"
 #include "clang/Rewrite/Core/Rewriter.h"
 
+#include "clang/Tooling/CompilationDatabase.h"
+#include "clang/Tooling/Refactoring.h"
+#include "clang/Tooling/Tooling.h"
 using namespace std;
 using namespace clang;
 
+//Rewriter rewrite;
 class CMessRecursiveASTVisitor : public RecursiveASTVisitor<CMessRecursiveASTVisitor>
 {
 	public :
 		CMessRecursiveASTVisitor(Rewriter &r) : rewrite(r) { }
 		Expr *VisitBinaryOperator(BinaryOperator *BO);
-		Rewriter rewrite;
+		Rewriter &rewrite;
 };
 
 Expr *CMessRecursiveASTVisitor::VisitBinaryOperator(BinaryOperator *BO)
@@ -43,36 +47,59 @@ Expr *CMessRecursiveASTVisitor::VisitBinaryOperator(BinaryOperator *BO)
 		 rewrite.InsertText(BO->getLHS()->getExprLoc(),BO->getOpcode() == BO_LAnd ? "AND(" : "OR(", true);
 		 rewrite.ReplaceText(BO->getOperatorLoc(), BO->getOpcodeStr().size(), ",");
 		 rewrite.InsertTextAfterToken(BO->getRHS()->getLocEnd(), ")");
+	//	 llvm::outs() << rewrite.ConvertToString(cast<Stmt>(BO)) << "\n";
 	 }
 	return BO;
 }
 
 class CMessASTConsumer : public ASTConsumer
 {
-	CMessASTConsumer(Rewriter &R) : crv(R) { }
-	virtual bool HandleTopLevelDecl(DeclGroupRef d);
-	CMessRecursiveASTVisitor crv;
+	public:
+		CMessASTConsumer(Rewriter &R) : crv(R) { }
+		virtual bool HandleTopLevelDecl(DeclGroupRef d);
+		CMessRecursiveASTVisitor crv;
 };
 
 bool CMessASTConsumer::HandleTopLevelDecl(DeclGroupRef d)
 {
 	  typedef DeclGroupRef::iterator iter;
-
+	  
 	  for (iter b = d.begin(), e = d.end(); b != e; ++b){
 		  crv.TraverseDecl(*b);
 	  }
 	  return true; // keep going
 }
 
+/*class CMessFrontendAction : public clang::ASTFrontendAction {
+	public:
+	  virtual clang::ASTConsumer *CreateASTConsumer(CompilerInstance &Compiler, llvm::StringRef InFile) {
+		   return new CMessASTConsumer(&rewrite);
+	  }
+};*/
+/*
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
-int main(int argc , char **argv)
+static cl::extrahelp MoreHelp("\nMore help text...");
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+*/
+
+//string str;
+//:raw_string_ostream out(str);
+
+int main(int argc , const char **argv)
 {
+/*
+	CommonOptionsParser OptionsParser(argc,argv,MyToolCategory);
+	ClangTool Tool(OptionsParser.getCompilations(),OptionsParser.getSourcePathList());
+	Tool.run(newFrontendActionFactory<CMessFrontendAction>());
+	return 0;
+*/
 	struct stat sb;
 	if(argc < 2){
 		llvm::errs() << "The right usage is ./bin/cmess <options> <filename>" << "\n";
 		return 1;
 	}
-	
+/*	
 	vector<char> vc;
 	for(unsigned int i = 0; i < strlen(argv[1]); i++ ){
 		if(argv[1][i] != '-'){
@@ -96,7 +123,7 @@ int main(int argc , char **argv)
 		}
 	}
 
-
+*/
 	std::string fileName(argv[argc - 1]);
 	
 	if (stat(fileName.c_str(), &sb) == -1) {
@@ -108,14 +135,12 @@ int main(int argc , char **argv)
 	compiler.createDiagnostics();
 
 	CompilerInvocation *Invocation = new CompilerInvocation;
-	CompilerInvocation::CreateFromArgs(*Invocation, argv + 1, argv + argc,
-	compiler.getDiagnostics());
+	CompilerInvocation::CreateFromArgs(*Invocation, argv + 1, argv + argc,compiler.getDiagnostics());
 	compiler.setInvocation(Invocation);
 
 	llvm::IntrusiveRefCntPtr<TargetOptions> pto( new TargetOptions());
 	pto->Triple = llvm::sys::getDefaultTargetTriple();
-	llvm::IntrusiveRefCntPtr<TargetInfo>
-	pti(TargetInfo::CreateTargetInfo(compiler.getDiagnostics(),pto.getPtr()));
+	llvm::IntrusiveRefCntPtr<TargetInfo> pti(TargetInfo::CreateTargetInfo(compiler.getDiagnostics(),pto.getPtr()));
 	compiler.setTarget(pti.getPtr());
 
 	compiler.createFileManager();
@@ -125,6 +150,33 @@ int main(int argc , char **argv)
 	
 	/*@TUDO why  headerSearchOptions.AddPath() is needed here*/
 
+	headerSearchOptions.AddPath("/usr/include/c++/4.6",
+	          clang::frontend::Angled,
+			            false,
+						          false);
+	  headerSearchOptions.AddPath("/usr/include/c++/4.6/i686-linux-gnu",
+	            clang::frontend::Angled,
+				          false,
+						            false);
+	    headerSearchOptions.AddPath("/usr/include/c++/4.6/backward",
+		          clang::frontend::Angled,
+				            false,
+							          false);
+		  headerSearchOptions.AddPath("/usr/local/include",
+		            clang::frontend::Angled,
+					          false,
+							            false);
+		    headerSearchOptions.AddPath("/usr/local/lib/clang/3.3/include",
+			          clang::frontend::Angled,
+					            false,
+								          false);
+			  headerSearchOptions.AddPath("/usr/include/i386-linux-gnu",
+			            clang::frontend::Angled,
+						          false,
+								            false);
+			    headerSearchOptions.AddPath("/usr/include",
+				          clang::frontend::Angled,
+						            false,false);
 	LangOptions langOpts;
 	langOpts.GNUMode = 1; 
 	langOpts.CXXExceptions = 1; 
@@ -132,7 +184,7 @@ int main(int argc , char **argv)
 	langOpts.Bool = 1; 
 	langOpts.CPlusPlus = 1; 
 	Invocation->setLangDefaults(langOpts,clang::IK_CXX,clang::LangStandard::lang_cxx0x);
-	compiler.createPreprocessor(clang::TU_Complete);
+	compiler.createPreprocessor(TU_Complete);
 	compiler.getPreprocessorOpts().UsePredefines = false;
 	compiler.createASTContext();
 
@@ -144,7 +196,7 @@ int main(int argc , char **argv)
 	compiler.getDiagnosticClient().BeginSourceFile(compiler.getLangOpts(),&compiler.getPreprocessor());
 	CMessASTConsumer astConsumer(Rewrite);
 
-	std::string outName (fileName);
+	std::string outName(fileName);
 	size_t ext = outName.rfind(".");
 	if (ext == std::string::npos)
 		ext = outName.length();
@@ -154,15 +206,15 @@ int main(int argc , char **argv)
 	std::string OutErrorInfo;
 	llvm::raw_fd_ostream outFile(outName.c_str(), OutErrorInfo, llvm::sys::fs::F_None);
 
-	if (OutErrorInfo.empty(){
+	if (OutErrorInfo.empty()){
 		ParseAST(compiler.getPreprocessor(), &astConsumer, compiler.getASTContext());
 		compiler.getDiagnosticClient().EndSourceFile();
 												     
 		outFile << "#define AND(a, b) a && b\n";
 		outFile << "#define OR(a, b) a || b\n\n";
-		const RewriteBuffer *RewriteBuf =
-		Rewrite.getRewriteBufferFor(compiler.getSourceManager().getMainFileID());
-		outFile << std::string(RewriteBuf->begin(), RewriteBuf->end());
+		const RewriteBuffer *RewriteBuf = Rewrite.getRewriteBufferFor(compiler.getSourceManager().getMainFileID());
+		//llvm::outs() << RewriteBuf->size() << "\n";
+	    outFile << std::string(RewriteBuf->begin(), RewriteBuf->end());
 	}else{
 		llvm::errs() << "Cannot open " << outName << " for writing\n";
 	}
@@ -170,7 +222,6 @@ int main(int argc , char **argv)
 	outFile.close();
 	return 0;
 }
-
 
 
 
